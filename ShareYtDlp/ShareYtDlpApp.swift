b0e716f259eb4a5b6321bf930373d125a89ea8bd
Tim Danner <tim@danners.us>
2022-03-12 16:10:47 -0600

    Initial Commit

---
+++ ShareYtDlp/ShareYtDlpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareYtDlpApp.swift
+//  ShareYtDlp
+//
+//  Created by Tim Danner on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ShareYtDlpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
